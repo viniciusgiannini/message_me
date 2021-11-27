c73a4db6cae4e7a97940982e918f41989601cbab
Vinicius Giannini <vinicius.lemegiannini@gmail.com>
2021-11-26 22:08:35 -0300

    Adjust the flash messages properly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(username: params[:session][:username])
         if user && user.authenticate(params[:session][:password])
-            session[:username] = user.id
+            session[:user_id] = user.id
             flash[:success] = "You have successfully logged in"
             redirect_to root_path
         else
